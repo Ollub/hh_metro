0ae2cab5965e758d632ba7324142792920b4ec44
Oleg <orlov.oficial@gmail.com>
2020-03-24 10:45:32 +0300

    Add HH response caching

--- metro/client.py
+++ metro/client.py
@@ -1,25 +1,36 @@
 from aiohttp import ClientSession, ClientTimeout
+
+from metro.logger import get_logger
 from metro.settings import Config
 
 
 class HH:
     url_msk_metro = 'https://api.hh.ru/metro/1'
     timeout = Config.REQUEST_TIMEOUT
+    cache = {}
+    logger = get_logger(__name__)
 
     @classmethod
     async def fetch_stations(cls):
-        resp = await cls._get_json_response(cls.url_msk_metro)
-        stations = await cls._get_stations_from_response(resp)
+        resp_json = await cls._get_json_response(cls.url_msk_metro)
+        stations = await cls._get_stations_from_response(resp_json)
         return stations
 
     @classmethod
     async def _get_json_response(cls, url, timeout=None):
         timeout = timeout or cls.timeout
         timeout = ClientTimeout(timeout)
-        async with ClientSession(raise_for_status=True) as session:
-            async with session.get(url, timeout=timeout) as resp:
-                response = await resp.json()
-                return response
+        headers = {'If-None-Match': cls.cache.get('Etag', '')}
+        async with ClientSession(raise_for_status=False) as session:
+            async with session.get(url, headers=headers, timeout=timeout) as resp:
+                cls.logger.info('Get response from HH API status={}'.format(resp.status))
+                if resp.status == 304:
+                    resp_json = cls.cache.get('resp_json')
+                else:
+                    cls.cache['Etag'] = resp.headers['Etag']
+                    resp_json = await resp.json()
+                    cls.cache['resp_json'] = resp_json
+                return resp_json
 
     @classmethod
     async def _get_stations_from_response(cls, metro_info):
